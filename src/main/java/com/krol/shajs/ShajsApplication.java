7432287b63f533808e69cb1d952e9ca794659208
Piotr <piotr.krol@billennium.pl>
2018-02-08 14:51:30 +0100

    initial commit

---
+++ src/main/java/com/krol/shajs/ShajsApplication.java
@@ -0,0 +1,12 @@
+package com.krol.shajs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShajsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShajsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/krol/shajs/ShajsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krol.shajs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShajsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
