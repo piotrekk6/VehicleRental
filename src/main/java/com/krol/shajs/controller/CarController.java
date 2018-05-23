cc0aa51e9a5255c7b95ff90404f3d8fd3b884d79
Piotr <piotrekk6@gmail.com>
2018-05-23 11:30:09 +0200

    changed imports

--- src/main/java/com/krol/shajs/controller/CarController.java
+++ src/main/java/com/krol/shajs/controller/CarController.java
@@ -3,10 +3,15 @@ package com.krol.shajs.controller;
 import com.krol.shajs.dto.CarDto;
 import com.krol.shajs.exceptions.NotFoundException;
 import com.krol.shajs.service.CarService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -33,7 +38,7 @@ public class CarController {
         carService.addCar(carDto);
     }
 
-    @PutMapping(value = "/editCar")
+    @ PutMapping(value = "/editCar")
     public void editCar(@RequestBody CarDto editCarDto) throws NotFoundException {
         carService.editCar(editCarDto);
     }
